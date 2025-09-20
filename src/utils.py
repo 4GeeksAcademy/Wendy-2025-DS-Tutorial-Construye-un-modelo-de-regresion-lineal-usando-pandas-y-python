822e5eb568da89d5f68b7db1b372011aace723ce
Wendy Chávez Gutiérrez <chavez.gutierrez.wendy3@gmail.com>
2025-09-20 20:44:01 +0000

    Project finished

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,36 @@
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 import pandas as pd
-
-# load the .env file variables
-load_dotenv()
-
-
-def db_connect():
-    import os
-    engine = create_engine(os.getenv('DATABASE_URL'))
-    engine.connect()
-    return engine
+import matplotlib.pyplot as plt
+import numpy as np
+import missingno as msno
+import seaborn as sns
+import statsmodels.api as sm
+import xgboost as xgb
+import joblib
+from matplotlib.colors import LinearSegmentedColormap
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from lazypredict.Supervised import LazyRegressor
+from lightgbm import LGBMRegressor
+from skopt import BayesSearchCV
+from scipy.stats import zscore
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from sklearn.impute import KNNImputer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, r2_score
+from sklearn.ensemble import BaggingRegressor
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, LabelEncoder
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.feature_selection import RFE
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+import utils as eda
+import importlib
